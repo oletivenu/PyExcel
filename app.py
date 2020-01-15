527b65debf3ceacd6c6c5f75fa545fdb6e884fd2
Naga Venu Babu  Oleti <N.Oleti@midisglobal.local>
2020-01-15 12:21:36 +0400

    app file commited

--- app.py
+++ app.py
@@ -3,23 +3,28 @@ import openpyxl
 # wb = openpyxl.Workbook()
 
 wb = openpyxl.load_workbook("transactions.xlsx")
-print(wb.sheetnames)
+# print(wb.sheetnames)
 
 sheet = wb["Sheet1"]
 
-cell = sheet["a1"]
-
-column = sheet["a"]
-
-cells = sheet["a:c"]
-
-sheet["a1:c3"]
-
-sheet[1:3]
-
+for row in range(1, 10):
+    cell = sheet.cell(row, 1)
+    print(cell.value)
 sheet.append([1, 2, 3])
-
 wb.save("transactions1.xlsx")
+
+# cell = sheet["a1"]
+
+# column = sheet["a"]
+
+# cells = sheet["a:c"]
+
+# sheet["a1:c3"]
+
+# sheet[1:3]
+
+# sheet.append([1, 2, 3])
+
 
 # print(column)
 
